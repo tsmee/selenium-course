ad0f6a94cb882235d061036ec8dd9d1835d89419
tsmee <draftpodium@gmail.com>
2018-12-08 17:14:09 +0700

    добавлен сценарий проверки наличия стикеров

---
+++ lesson04/test_ducks.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture(scope="class")
+def setup(request):
+    print("initiating chrome driver")
+    driver = webdriver.Firefox()
+    request.cls.driver = driver
+    driver.get("http://localhost/litecart/")
+    driver.maximize_window()
+    yield driver
+    driver.close()
+
+
+@pytest.mark.usefixtures("setup")
+class TestDucks:
+    def test_ducks(self):
+        ducks = self.driver.find_elements_by_xpath("//li[@class='product column shadow hover-light']")
+        for n in ducks:
+            assert len(n.find_elements_by_xpath(".//div[contains(@class,'sticker')]")) == 1
+            sticker_text = n.find_element_by_xpath(".//div[contains(@class,'sticker')]").text
+            ducks_name = n.find_element_by_xpath(".//div[@class='name']").text
+            print(ducks_name + "'s " "sticker type is " + sticker_text)
+
