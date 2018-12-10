4e3356488e57f1a4f76c920a5727f0f025edc62c
tsmee <draftpodium@gmail.com>
2018-12-10 21:47:51 +0700

    выбран более удобный локатор для поиска карточки товара

--- lesson04/test_ducks.py
+++ lesson04/test_ducks.py
@@ -8,14 +8,14 @@ def setup(request):
     request.cls.driver = driver
     driver.get("http://localhost/litecart/")
     driver.maximize_window()
-    yield driver
-    driver.close()
+    # yield driver
+    # driver.close()
 
 
 @pytest.mark.usefixtures("setup")
 class TestDucks:
     def test_ducks(self):
-        ducks = self.driver.find_elements_by_xpath("//li[@class='product column shadow hover-light']")
+        ducks = self.driver.find_elements_by_css_selector("li.product")
         for n in ducks:
             assert len(n.find_elements_by_xpath(".//div[contains(@class,'sticker')]")) == 1
             sticker_text = n.find_element_by_xpath(".//div[contains(@class,'sticker')]").text
